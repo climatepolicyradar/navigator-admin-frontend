3e876f40359a7fefb5f065d58d554129cb94f989
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-09-26 12:12:57 +0100

    Add types for family for UNFCCC and CCLW sources

--- src/interfaces/Family.ts
+++ src/interfaces/Family.ts
@@ -13,3 +13,41 @@ export interface IFamily {
   documents: string[]
   collections: string[]
 }
+
+interface IFamilyBase {
+  title: string
+  summary: string
+  geography: string
+  category: string
+  status: string
+  slug: string
+  events: string[]
+  published_date: string
+  last_updated_date: string
+  documents: string[]
+  collections: string[]
+}
+
+interface IUNFCCCMetadata {
+  author: string[]
+  author_type: string[]
+}
+
+export interface IUNFCCCFamily extends IFamilyBase {
+  metadata: IUNFCCCMetadata
+  organisation: 'UNFCCC'
+}
+
+interface ICCLWMetadata {
+  topic: string[]
+  hazard: string[]
+  sector: string[]
+  keyword: string[]
+  framework: string[]
+  instrument: string[]
+}
+
+export interface ICCLWFamily extends IFamilyBase {
+  metadata: ICCLWMetadata
+  organisation: 'CCLW'
+}
