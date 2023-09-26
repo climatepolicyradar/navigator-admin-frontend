92f46fa534244937d04303bf1cf7f788bd2e6d0b
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-09-26 12:12:39 +0100

    Add types for config endpoint

---
+++ src/interfaces/Config.ts
@@ -0,0 +1,40 @@
+interface IConfigGeographyNode {
+  id: number
+  display_value: string
+  slug: string
+  value: string
+  type: string
+  parent_id: number
+}
+
+interface IConfigGeography {
+  node: IConfigGeographyNode
+  children: IConfigGeographyNode[]
+}
+
+interface IConfigMeta {
+  allow_any?: boolean
+  allow_blanks: boolean
+  allowed_values: string[]
+}
+
+export interface IConfig {
+  geographies: IConfigGeography[]
+  languages: {
+    [key: string]: string
+  }
+  taxonomies: {
+    CCLW: {
+      topic: IConfigMeta
+      hazard: IConfigMeta
+      sector: IConfigMeta
+      keyword: IConfigMeta
+      framework: IConfigMeta
+      instrument: IConfigMeta
+    }
+    UNFCCC: {
+      author: IConfigMeta
+      author_type: IConfigMeta
+    }
+  }
+}
