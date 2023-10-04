a257b6f830b1925f1ef698fc8d3997901758b7c5
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-10-04 15:00:23 +0100

    Adding multi-select to family form

--- src/components/forms/FamilyForm.tsx
+++ src/components/forms/FamilyForm.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { useForm, SubmitHandler } from 'react-hook-form'
+import { useForm, SubmitHandler, Controller } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup'
 
@@ -16,7 +16,7 @@ import {
   Input,
   Radio,
   RadioGroup,
-  Select,
+  // Select,
   Textarea,
   VStack,
   Text,
@@ -25,19 +25,23 @@ import {
   FormErrorMessage,
   useToast,
   SkeletonText,
+  Divider,
+  AbsoluteCenter,
 } from '@chakra-ui/react'
+import { Select, ChakraStylesConfig } from 'chakra-react-select'
 import useCollections from '@/hooks/useCollections'
 import { Loader } from '../Loader'
 import { getCountries } from '@/utils/extractNestedGeographyData'
 
-interface IFamilyFormTEMP {
+interface IFamilyForm {
   import_id: string
   title: string
   summary: string
   geography: string
   category: string
-  // metadata: string
   organisation: string
+  author?: string
+  author_type?: string
 }
 
 const schema = yup
@@ -48,8 +52,26 @@ const schema = yup
     geography: yup.string().required(),
     category: yup.string().required(),
     organisation: yup.string().required(),
+    author: yup.string().when('organisation', {
+      is: 'UNFCCC',
+      then: (schema) => schema.required(),
+    }),
+    author_type: yup.string().when('organisation', {
+      is: 'UNFCCC',
+      then: (schema) => schema.required(),
+    }),
+    topic: yup.array().optional(),
+    // hazard: yup.array().of(yup.string()),
+    // sector: yup.array().of(yup.string()),
+    // keyword: yup.array().of(yup.string()),
+    // framework: yup.array().of(yup.string()),
+    // instrument: yup.array().of(yup.string()),
   })
   .required()
+
+const generateOptions = (values?: string[]) => {
+  return values?.map((value) => ({ value, label: value })) || []
+}
 
 export const FamilyForm = () => {
   const { config, error: configError, loading: configLoading } = useConfig()
@@ -63,42 +85,53 @@ export const FamilyForm = () => {
   const [formError, setFormError] = useState<IError | null | undefined>()
   const {
     register,
+    watch,
     handleSubmit,
+    control,
     formState: { errors, isSubmitting },
   } = useForm({
     resolver: yupResolver(schema),
   })
 
-  // const countries = useMemo(() => getCountries(config?.geographies), [config])
+  const watchOrganisation = watch('organisation')
 
-  const handleFamilyCreate = async (family: IFamilyFormTEMP) => {
+  const handleFamilyCreate = (family: IFamilyForm) => {
     setFormError(null)
 
-    await createFamily(family)
-      .then(() => {
-        toast.closeAll()
-        toast({
-          title: 'Family has been successfully created',
-          status: 'success',
-          position: 'top',
-        })
-      })
-      .catch((error: IError) => {
-        setFormError(error)
-        toast({
-          title: 'Family has not been created',
-          description: error.message,
-          status: 'error',
-          position: 'top',
-        })
-      })
+    console.log(family)
+
+    // await createFamily(family)
+    //   .then(() => {
+    //     toast.closeAll()
+    //     toast({
+    //       title: 'Family has been successfully created',
+    //       status: 'success',
+    //       position: 'top',
+    //     })
+    //   })
+    //   .catch((error: IError) => {
+    //     setFormError(error)
+    //     toast({
+    //       title: 'Family has not been created',
+    //       description: error.message,
+    //       status: 'error',
+    //       position: 'top',
+    //     })
+    //   })
   }
 
-  const onSubmit: SubmitHandler<IFamilyFormTEMP> = (data) =>
+  const onSubmit: SubmitHandler<IFamilyForm> = (data) =>
     handleFamilyCreate(data)
 
   const canLoadForm =
     !configLoading && !collectionsLoading && !configError && !collectionsError
+
+  const chakraStyles: ChakraStylesConfig = {
+    container: (provided, _state) => ({
+      ...provided,
+      background: 'white',
+    }),
+  }
 
   return (
     <>
@@ -134,7 +167,7 @@ export const FamilyForm = () => {
       )}
       {canLoadForm && (
         <form onSubmit={handleSubmit(onSubmit)}>
-          <VStack gap="4" mb={4} align={'stretch'}>
+          <VStack gap="4" mb={12} align={'stretch'}>
             {formError && (
               <Box>
                 <Text color={'red.500'}>{formError.message}</Text>
@@ -159,17 +192,30 @@ export const FamilyForm = () => {
               <FormLabel>Summary</FormLabel>
               <Textarea height={'300px'} bg="white" {...register('summary')} />
             </FormControl>
+            <FormControl>
+              <FormLabel>Collection</FormLabel>
+              {/* <Select background="white">
+                <option value="">Please select</option>
+                {collections?.map((collection) => (
+                  <option
+                    key={collection.import_id}
+                    value={collection.import_id}
+                  >
+                    {collection.title}
+                  </option>
+                ))}
+              </Select> */}
+            </FormControl>
             <FormControl isRequired>
               <FormLabel>Geography</FormLabel>
-              <Select background="white" {...register('geography')}>
+              {/* <Select background="white" {...register('geography')}>
                 <option value="">Please select</option>
-                <option value="SWE">Sweden</option>
                 {getCountries(config?.geographies).map((country) => (
                   <option key={country.id} value={country.value}>
                     {country.display_value}
                   </option>
                 ))}
-              </Select>
+              </Select> */}
             </FormControl>
             <FormControl isRequired as="fieldset" isInvalid={!!errors.category}>
               <FormLabel as="legend">Category</FormLabel>
@@ -221,13 +267,138 @@ export const FamilyForm = () => {
               </RadioGroup>
               <FormErrorMessage>Please select an organisation</FormErrorMessage>
             </FormControl>
-            <FormControl>
-              <FormLabel>Collection</FormLabel>
-              <Select background="white">
-                <option value="">Please select</option>
-                <option value="CCLW.collection.1687.0">CO2 Act</option>
-              </Select>
-            </FormControl>
+            {!!watchOrganisation && (
+              <Box position="relative" padding="10">
+                <Divider />
+                <AbsoluteCenter bg="gray.50" px="4">
+                  Metadata
+                </AbsoluteCenter>
+              </Box>
+            )}
+            {watchOrganisation === 'UNFCCC' && (
+              <>
+                <FormControl isRequired>
+                  <FormLabel>Author</FormLabel>
+                  <Input bg="white" {...register('author')} />
+                </FormControl>
+                <FormControl
+                  isRequired
+                  as="fieldset"
+                  isInvalid={!!errors.author_type}
+                >
+                  <FormLabel as="legend">Author type</FormLabel>
+                  <RadioGroup>
+                    <HStack gap={4}>
+                      {config?.taxonomies.UNFCCC.author_type.allowed_values.map(
+                        (authorType) => (
+                          <Radio
+                            bg="white"
+                            value={authorType}
+                            {...register('author_type')}
+                            key={authorType}
+                          >
+                            {authorType}
+                          </Radio>
+                        ),
+                      )}
+                    </HStack>
+                  </RadioGroup>
+                  <FormErrorMessage>
+                    Please select an organisation
+                  </FormErrorMessage>
+                </FormControl>
+              </>
+            )}
+            {watchOrganisation === 'CCLW' && (
+              <>
+                <Controller
+                  control={control}
+                  name="topic"
+                  render={({ field }) => {
+                    return (
+                      <FormControl>
+                        <FormLabel>Topics</FormLabel>
+                        <Select
+                          chakraStyles={chakraStyles}
+                          isClearable={false}
+                          isMulti={true}
+                          isSearchable={true}
+                          options={generateOptions(
+                            config?.taxonomies.CCLW.topic.allowed_values,
+                          )}
+                          {...field}
+                        />
+                      </FormControl>
+                    )
+                  }}
+                />
+                {/* <FormControl>
+                  <FormLabel>Hazards</FormLabel>
+                  <Select background="white" {...register('hazard')}>
+                    <option value="">Please select</option>
+                    {config?.taxonomies.CCLW.hazard.allowed_values.map(
+                      (option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ),
+                    )}
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Sectors</FormLabel>
+                  <Select background="white" {...register('sector')}>
+                    <option value="">Please select</option>
+                    {config?.taxonomies.CCLW.sector.allowed_values.map(
+                      (option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ),
+                    )}
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Keywords</FormLabel>
+                  <Select background="white" {...register('keyword')}>
+                    <option value="">Please select</option>
+                    {config?.taxonomies.CCLW.keyword.allowed_values.map(
+                      (option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ),
+                    )}
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Frameworks</FormLabel>
+                  <Select background="white" {...register('framework')}>
+                    <option value="">Please select</option>
+                    {config?.taxonomies.CCLW.framework.allowed_values.map(
+                      (option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ),
+                    )}
+                  </Select>
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Instruments</FormLabel>
+                  <Select background="white" {...register('instrument')}>
+                    <option value="">Please select</option>
+                    {config?.taxonomies.CCLW.instrument.allowed_values.map(
+                      (option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ),
+                    )}
+                  </Select>
+                </FormControl> */}
+              </>
+            )}
           </VStack>
 
           <ButtonGroup>
