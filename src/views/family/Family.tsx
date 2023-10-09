e4d290a1ba2a803e23e5a6fa8be9a781021e2afc
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-10-09 16:46:47 +0100

    Add loading and handling for single family

--- src/views/family/Family.tsx
+++ src/views/family/Family.tsx
@@ -1,10 +1,15 @@
-import { useParams } from 'react-router-dom'
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { useParams, Link } from 'react-router-dom'
+import { Box, Heading, Text, Button, SkeletonText } from '@chakra-ui/react'
 import { FamilyForm } from '@/components/forms/FamilyForm'
+import useFamily from '@/hooks/useFamily'
+import { Loader } from '@/components/Loader'
 
 export default function Family() {
   const { importId } = useParams()
-  console.log(importId)
+  const { family, loading, error } = useFamily(importId)
+
+  const canLoadForm = !loading && !error
+  // const canLoadForm = false
 
   return (
     <>
@@ -16,7 +21,31 @@ export default function Family() {
         indicates required fields
       </Text>
       <Box my={4} p={4} bg={'gray.50'} boxShadow="base">
-        <FamilyForm />
+        {error && (
+          <>
+            <Text color={'red.500'}>{error.message}</Text>
+            <Text fontSize="xs" color={'gray.500'}>
+              {error.detail}
+            </Text>
+            <Button as={Link} to={'/families'} colorScheme="blue" mt={4}>
+              Back to families
+            </Button>
+          </>
+        )}
+        {loading && (
+          <>
+            <Box padding="4" bg="white">
+              <Loader />
+              <SkeletonText
+                mt="4"
+                noOfLines={12}
+                spacing="4"
+                skeletonHeight="2"
+              />
+            </Box>
+          </>
+        )}
+        {canLoadForm && <FamilyForm family={family ?? undefined} />}
       </Box>
     </>
   )
