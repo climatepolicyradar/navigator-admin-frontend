9a5b79223c32bd892a5c70e749c239674b8eb7d4
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-10-16 17:58:20 +0100

    Add summary content to dashboard

--- src/views/dashboard/Dashboard.tsx
+++ src/views/dashboard/Dashboard.tsx
@@ -1,21 +1,69 @@
 import { Link as ReactRouterLink } from 'react-router-dom'
-import { Heading, Link, Stack, Text } from '@chakra-ui/react'
+import {
+  Heading,
+  Link,
+  Stack,
+  Stat,
+  StatLabel,
+  StatNumber,
+  StatHelpText,
+  SimpleGrid,
+  Card,
+  CardBody,
+} from '@chakra-ui/react'
+import useSummary from '@/hooks/useSummary'
+
+type TCardLink = {
+  to: string
+  children: React.ReactNode
+}
+
+const CardLink = ({ to, children }: TCardLink) => (
+  <Link as={ReactRouterLink} to={to}>
+    <Card>
+      <CardBody>{children}</CardBody>
+    </Card>
+  </Link>
+)
 
 const Dashboard = () => {
+  const { summary, loading, error } = useSummary()
+
   return (
     <Stack spacing={4}>
       <Heading as={'h1'}>Dashboard</Heading>
-      <Text>
-        Content for the dashboard TBC, but for now{' '}
-        <Link as={ReactRouterLink} color="teal.500" to="/families">
-          click here to view Families
-        </Link>
-      </Text>
-      <Text>
-        <Link as={ReactRouterLink} color="teal.500" to="/collections">
-          Click here to view Collections
-        </Link>
-      </Text>
+      {!loading && !error && (
+        <SimpleGrid columns={2} gap="4">
+          <CardLink to="/families">
+            <Stat>
+              <StatLabel>Families</StatLabel>
+              <StatNumber>{summary?.n_families}</StatNumber>
+              <StatHelpText>Click here to view Families</StatHelpText>
+            </Stat>
+          </CardLink>
+          <CardLink to="/documents">
+            <Stat>
+              <StatLabel>Documents</StatLabel>
+              <StatNumber>{summary?.n_documents}</StatNumber>
+              <StatHelpText>Click here to view Documents</StatHelpText>
+            </Stat>
+          </CardLink>
+          <CardLink to="/collections">
+            <Stat>
+              <StatLabel>Collections</StatLabel>
+              <StatNumber>{summary?.n_collections}</StatNumber>
+              <StatHelpText>Click here to view collections</StatHelpText>
+            </Stat>
+          </CardLink>
+          <CardLink to="/events">
+            <Stat>
+              <StatLabel>Events</StatLabel>
+              <StatNumber>{summary?.n_events}</StatNumber>
+              <StatHelpText>Click here to view Events</StatHelpText>
+            </Stat>
+          </CardLink>
+        </SimpleGrid>
+      )}
     </Stack>
   )
 }
