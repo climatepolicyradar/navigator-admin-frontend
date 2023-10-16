43afde8f34446e783e7616f0e7e796fae550ef17
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-10-16 17:58:10 +0100

    Add summary handlers

---
+++ src/hooks/useSummary.ts
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react'
+
+import { IError, ISummary } from '@/interfaces'
+import { getSummary } from '@/api/Summary'
+
+let cache: ISummary | null = null
+
+const useSummary = () => {
+  const [summary, setConfig] = useState<ISummary | null>(null)
+  const [error, setError] = useState<IError | null | undefined>()
+  const [loading, setLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    let ignore = false
+    setLoading(true)
+
+    if (cache) {
+      setConfig(cache)
+      setLoading(false)
+      return
+    }
+
+    getSummary()
+      .then(({ response }) => {
+        if (!ignore) {
+          cache = response
+          setConfig(response)
+        }
+      })
+      .catch((error: IError) => {
+        setError(error)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+    return () => {
+      ignore = true
+    }
+  }, [])
+
+  return { summary, error, loading }
+}
+
+export default useSummary
