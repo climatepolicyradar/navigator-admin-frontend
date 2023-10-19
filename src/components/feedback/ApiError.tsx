f13db5c6539433bd0239fab27e35a3ed07a78499
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-10-19 11:28:31 +0100

    Allow for flexibility of error messaging

--- src/components/feedback/ApiError.tsx
+++ src/components/feedback/ApiError.tsx
@@ -3,15 +3,17 @@ import { Box, Text } from '@chakra-ui/react'
 import { IError } from '@/interfaces'
 
 type TProps = {
-  error: IError
+  error?: IError
+  message?: string
+  detail?: string
 }
 
-export const ApiError = ({ error }: TProps) => {
+export const ApiError = ({ error, message = '', detail = '' }: TProps) => {
   return (
     <Box padding="4" bg="white">
-      <Text color={'red.500'}>{error.message}</Text>
+      <Text color={'red.500'}>{error?.message || message}</Text>
       <Text fontSize="xs" color={'gray.500'}>
-        {error.detail}
+        {error?.detail || detail}
       </Text>
     </Box>
   )
