25ee890fdf458c3f36d638a2c30f61898f0497e8
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-10-19 16:18:36 +0100

    Update document schema

--- src/schemas/documentSchema.ts
+++ src/schemas/documentSchema.ts
@@ -5,9 +5,15 @@ export const documentSchema = yup
     family_import_id: yup.string().required(),
     variant_name: yup.string().optional(),
     role: yup.string().required(),
-    type: yup.string().optional(),
+    type: yup.string().required(),
     title: yup.string().required(),
     source_url: yup.string().required(),
     user_language_name: yup.string().required(),
+    // user_language_name: yup
+    //   .object<{ label: string; value: string }>({
+    //     label: yup.string().required(),
+    //     value: yup.string().required(),
+    //   })
+    //   .required(),
   })
   .required()
