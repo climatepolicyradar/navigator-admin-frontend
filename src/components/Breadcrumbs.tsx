771ec69bc230c5aa72ea41fe78809e5e6d94b664
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2023-10-23 15:50:03 +0100

    Fix lint errors unused vars

--- src/components/Breadcrumbs.tsx
+++ src/components/Breadcrumbs.tsx
@@ -1,18 +1,7 @@
-import { useEffect, useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
 
 export const BreadCrumbs = () => {
-  const { pathname } = useLocation()
-  const [currentPage, setCurrentPage] = useState<string | null | undefined>()
-
-  useEffect(() => {
-    if (pathname.includes('edit'))
-      return setCurrentPage(pathname.split('/').reverse()[1])
-    if (pathname.includes('new')) return setCurrentPage('New family')
-    return setCurrentPage(null)
-  }, [pathname])
-
   return (
     <Breadcrumb fontSize="sm">
       <BreadcrumbItem>
