4edf366241c1afdf9a5786c1e5c3c131847507bb
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-02-13 15:17:26 +0000

    Improved commenting.

--- env.sh
+++ env.sh
@@ -1,12 +1,11 @@
-#!/bin/sh
-#  Make sure to use the prefix MY_APP_ if you have any other prefix in env.production file variable name replace it with MY_APP_
+#!/bin/bash
+#  Make sure to use the prefix MY_APP_.
+#If you have any other prefix in env.production file variable name replace it with MY_APP_.
 for i in $(env | grep MY_APP_); do
 	key=$(echo "${i}" | cut -d '=' -f 1)
 	value=$(echo "${i}" | cut -d '=' -f 2-)
 	echo "${key}"="${value}"
-	# sed All files
-	# find /usr/share/nginx/html -type f -exec sed -i "s|${key}|${value}|g" '{}' +
 
-	# sed JS and CSS only
+	# Replaced hardcoded values with value of env variable injected at runtime.
 	find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|${key}|${value}|g" '{}' +
 done
