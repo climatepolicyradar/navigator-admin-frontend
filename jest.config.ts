8e6a02dc3da42fe6a27021c7a00da5c000b5ba34
Adrián Sacristán Ibáñez <adriansacristan1993@gmail.com>
2024-02-26 10:18:28 +0000

    Pdct 833/data added column for families (#42)
    * PDCT-833 add column and tests
    
    * PDCT-833 add column and tests
    
    * PDCT-833 Complete tests

--- jest.config.ts
+++ jest.config.ts
@@ -10,4 +10,24 @@ export default {
     '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
     '^@utils/(.*)$': '<rootDir>/src/utils/$1',
   },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: 'node_modules/ts-jest-mock-import-meta',
+              options: {
+                metaObjectReplacement: { url: 'https://www.url.com' },
+              },
+            },
+          ],
+        },
+      },
+    ],
+  },
 }
