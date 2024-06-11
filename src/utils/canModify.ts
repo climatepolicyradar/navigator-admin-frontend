2d7257cff20a5d3873cf4b09fc2020c34aba054f
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-06-11 10:48:42 +0100

    Add isSuperuser check and correct existing var types

--- src/utils/canModify.ts
+++ src/utils/canModify.ts
@@ -1,14 +1,17 @@
 export const canModify = (
-  organisation: string,
+  organisation: string | null,
+  isSuperuser: boolean,
   userAccess?:
     | never[]
+    | null
     | {
         [key: string]: {
           is_admin: boolean
         }
       },
 ) => {
-  if (!organisation) return true
-  if (!userAccess) return false
+  if (isSuperuser === true) return true
+  if (organisation === null) return true
+  if (!userAccess || userAccess === null) return false
   return organisation in userAccess
 }
