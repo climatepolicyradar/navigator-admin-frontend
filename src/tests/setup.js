3c06144c51b15d47be3be5c1c9850379b13898f9
Anna Pokorska <anna@climatepolicyradar.org>
2024-07-30 13:26:30 +0100

    Add jwt-encode to make it clearer what auth token is being set before each test

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -2,16 +2,14 @@ import { cleanup } from '@testing-library/react'
 import { reset } from './mocks/repository.ts'
 import { server } from './mocks/server.ts'
 import * as matchers from '@testing-library/jest-dom/matchers'
+import sign from 'jwt-encode'
 
 expect.extend(matchers)
 
 // Establish API mocking before all tests.
 beforeAll(() => {
   server.listen()
-  localStorage.setItem(
-    'token',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc19zdXBlcnVzZXIiOnRydWV9.d72FxLE8LDpEX5_9_pF0NR8k1W1PZe73Nrd2zi2WJJ0',
-  )
+  localStorage.setItem('token', sign({ is_superuser: true }, ''))
 })
 
 // Reset any request handlers that we may add during the tests,
