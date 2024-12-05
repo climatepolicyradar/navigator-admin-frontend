a8d7fabe9e23c7d59a706076bf73b6d6b85d77ab
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-12-05 12:31:49 +0000

    Fix schema

--- src/schemas/familySchema.ts
+++ src/schemas/familySchema.ts
@@ -1,4 +1,3 @@
-import { IConfigCorpus } from '@/interfaces'
 import * as yup from 'yup'
 
 // Base schema for core family fields
@@ -6,11 +5,16 @@ export const baseFamilySchema = yup
   .object({
     title: yup.string().required('Title is required'),
     summary: yup.string().required('Summary is required'),
-    geography: yup.string().required('Geography is required'),
+    geography: yup
+      .object({
+        label: yup.string().required(),
+        value: yup.string().required(),
+      })
+      .required('Geography is required'),
     category: yup.string().required('Category is required'),
     corpus: yup.object({
-      label: yup.string().required('Corpus label is required'),
-      value: yup.string().required('Corpus value is required'),
+      label: yup.string().required(),
+      value: yup.string().required(),
     }),
     collections: yup.array().optional(),
   })
