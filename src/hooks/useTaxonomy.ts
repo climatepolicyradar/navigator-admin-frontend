6573d92a8bd65c35ba438b23407310b469a5732a
Katy Baulch <46493669+katybaulch@users.noreply.github.com>
2024-12-05 16:10:55 +0000

    Remove AF metadata

--- src/hooks/useTaxonomy.ts
+++ src/hooks/useTaxonomy.ts
@@ -11,7 +11,6 @@ const useTaxonomy = (
       return corpus_taxonomy as IConfigTaxonomyCCLW
     else if (corpus_type === 'Intl. agreements')
       return corpus_taxonomy as IConfigTaxonomyUNFCCC
-    else if (corpus_type === 'AF') return corpus_taxonomy
     else return corpus_taxonomy
   }, [corpus_type, corpus_taxonomy])
 }
