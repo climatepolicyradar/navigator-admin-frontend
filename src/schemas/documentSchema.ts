3dc99cbdc492c791ca6ad72324b31603be125d22
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-12-10 11:57:53 +0000

    refactor: update yup schema on document form
    as we know that mcf taxonomy will not have role or type on the metadata we are making this field optional in the validation

--- src/schemas/documentSchema.ts
+++ src/schemas/documentSchema.ts
@@ -4,8 +4,8 @@ export const documentSchema = yup
   .object({
     family_import_id: yup.string().required(),
     variant_name: yup.string().optional(),
-    role: yup.string().required(),
-    type: yup.string().required(),
+    role: yup.string().optional(),
+    type: yup.string().optional(),
     title: yup.string().required(),
     source_url: yup.string().url().optional(),
     user_language_name: yup
