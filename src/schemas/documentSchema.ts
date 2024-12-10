fb0c0ed81db19aabd5412780544b28f77da4e744
Osneil Drakes <osneildrakes@Osneils-MBP.communityfibre.co.uk>
2024-12-10 16:33:47 +0000

    fix: conditionally validate field depending on whether it is present in the form

--- src/schemas/documentSchema.ts
+++ src/schemas/documentSchema.ts
@@ -4,8 +4,16 @@ export const documentSchema = yup
   .object({
     family_import_id: yup.string().required(),
     variant_name: yup.string().optional(),
-    role: yup.string().optional(),
-    type: yup.string().optional(),
+    role: yup.string().when('$isRoleRequired', {
+      is: true,
+      then: (schema) => schema.required(),
+      otherwise: (schema) => schema.notRequired(),
+    }),
+    type: yup.string().when('$isTypeRequired', {
+      is: true,
+      then: (schema) => schema.required(),
+      otherwise: (schema) => schema.notRequired(),
+    }),
     title: yup.string().required(),
     source_url: yup.string().url().optional(),
     user_language_name: yup
