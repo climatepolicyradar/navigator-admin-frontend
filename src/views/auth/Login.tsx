acc3edf4644648475a05d4f3c45cae66a03cda43
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-01-07 17:30:57 +0000

    Allow user to access login page when logged in

--- src/views/auth/Login.tsx
+++ src/views/auth/Login.tsx
@@ -1,6 +1,8 @@
 import { LoginForm } from '@/components/forms/LoginForm'
 
 export default function Login() {
+  // TODO: redirect user to the index page if they are already authenticated
+  // const { token } = useAuth()
   return (
     <>
       <LoginForm />
