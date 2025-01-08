9ee4fabe8d150e097d080124f99e32b6a624b8dc
Pat Fawbert Mills <PatFawbertMills@users.noreply.github.com>
2025-01-08 14:58:04 +0000

    Update chakra-ui/react to v3 and add snippets

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 
 import AuthProvider from './providers/AuthProvider'
 import Routes from './routes'
@@ -9,7 +9,7 @@ import './styles/main.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider value={defaultSystem}>
       <AuthProvider>
         <Routes />
       </AuthProvider>
